10a8bab48777b4852443840c93c55316d9e089c8
zmercury <messiniki@outlook.com>
2026-01-12 16:01:55 +0545

    changes: cmdheight changed to 1

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,3 +1,18 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+
+vim.opt.cursorline = false
+vim.opt.cmdheight = 1
+
+-- LazyVim / plugins may set cmdheight=0 when using Noice.
+-- Force the classic bottom cmdline back *after* Lazy has finished (User VeryLazy).
+vim.api.nvim_create_autocmd("User", {
+  pattern = "VeryLazy",
+  callback = function()
+    -- schedule so this runs after any other VeryLazy handlers
+    vim.schedule(function()
+      vim.o.cmdheight = 1
+    end)
+  end,
+})
