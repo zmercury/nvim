ef99526317a03edf498cd93819c28fe64aab5b62
zmercury <messiniki@outlook.com>
2026-01-17 06:55:12 +0545

    Changes: Changed clock to 12 hrs in Lualine

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -4,6 +4,12 @@ return {
     opts = function(_, opts)
       opts.options = opts.options or {}
       opts.options.theme = "gruvbox"
+
+      opts.sections.lualine_z = {
+        function()
+          return os.date("%I:%M %p") -- 12-hour format with AM/PM
+        end,
+      }
     end,
   },
 }
