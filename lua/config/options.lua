dc01db678f21394a8ca0d6661341d4abc7d80438
zmercury <messiniki@outlook.com>
2026-01-24 20:04:47 +0545

    fix: changed the default shell to pwsh

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,6 +1,7 @@
--- Options are automatically loaded before lazy.nvim startup
--- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
--- Add any additional options here
+vim.opt.shell = "pwsh"
+vim.opt.shellcmdflag = "-NoLogo -NoProfile -ExecutionPolicy RemoteSigned -Command"
+vim.opt.shellquote = ""
+vim.opt.shellxquote = ""
 
 vim.opt.cursorline = false
 vim.opt.cmdheight = 1
