c836691e7146465679b95e641c8bb60d9678d282
zmercury <messiniki@outlook.com>
2026-01-24 20:06:53 +0545

    fix: removed conflicting keymaps j and k

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -5,7 +5,7 @@
 -- Use <leader>e for netrw instead of Snacks explorer
 -- This opens the netrw explorer for the current directory
 vim.keymap.set("n", "<leader>e", vim.cmd.Ex, { desc = "File Explorer (netrw)" })
-vim.keymap.set("n", "<C-k>", "<C-u>", { desc = "Up" })
-vim.keymap.set("n", "<C-j>", "<C-d>", { desc = "Down" })
+-- vim.keymap.set("n", "<C-k>", "<C-u>", { desc = "Up" })
+-- vim.keymap.set("n", "<C-j>", "<C-d>", { desc = "Down" })
 vim.keymap.set("n", "$$", "$A")
 vim.keymap.set("n", "<C-a>", "ggVG", { noremap = true, silent = true })
